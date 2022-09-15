5d0a931deac9ef4cf090c7a0b02fe6f9475196dc
KolyaGnev <baptiste.feutelais@gmail.com>
2022-09-15 18:13:46 +0200

    Mise à jour code

---
+++ assets/js/fichier.js
@@ -0,0 +1,8 @@
+var reponse1 = window.prompt("Saisissez votre nom");
+var reponse2 = window.prompt("Saisissez votre prenom");
+if (window.confirm("Etes-vous un homme ?") == true) 
+{ 
+   var sex="Monsieur"
+
+}
+window.alert("Bonjour "+sex+" "+reponse1+" "+reponse2+"\n\nBienvenue sur notre site web !")
